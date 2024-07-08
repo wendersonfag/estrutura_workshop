3d072e287de5c9f9480f48f9e44b2929cd0d206d
Wenderson <wenderson_fagundes@hotmail.com>
2024-07-07 23:08:50 -0300

    feat: adicioando extratação

--- app/pipeline/extract.py
+++ app/pipeline/extract.py
@@ -12,6 +12,8 @@ args: input_path (str): caminho da pasta com os arquivos
 return: lista de dataframes
 """
 
+
+
 path = "data/input"
 
 def extract_from_excel(path: str) -> List[pd.DataFrame]:
@@ -20,7 +22,7 @@ def extract_from_excel(path: str) -> List[pd.DataFrame]:
     data_frame_list = []
     for file in all_files:
         data_frame_list.append(pd.read_excel(file))
-
+    
 
     return data_frame_list
 
