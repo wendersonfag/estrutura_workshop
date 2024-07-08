abeb335688e3221457fe0c2e0914748399ad1bd1
Wenderson <wenderson_fagundes@hotmail.com>
2024-07-07 23:34:26 -0300

    feat: file test

--- app/pipeline/transform.py
+++ app/pipeline/transform.py
@@ -5,4 +5,4 @@ função para transformar uma lista de dataframes em único dataframe
 """
 
 def concat_data_frames(data_frames_list: List[pd.DataFrame]) -> pd.DataFrame:
-    return pd.concat(data_frames_list)
+    return pd.concat(data_frames_list, ignore_index=True)
--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,3 +1,11 @@
 import pandas as pd
+from app.pipeline.transform import concat_data_frames
 
+df_1 = pd.DataFrame({'col1': [1,2], 'col2': [3,4]})
+df_2 = pd.DataFrame({'col1': [5,6], 'col2': [7,8]})
 
+def testar_a_concatenacao_da_lista_de_dataframe():
+    arrange = pd.concat([df_1,df_2],ignore_index=True)
+
+    act = concat_data_frames([df_1,df_2])
+    assert arrange  == act 
