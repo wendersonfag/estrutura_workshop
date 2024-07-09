9c05f241631dd619d33107d855e773fdc925187c
Wenderson <wenderson_fagundes@hotmail.com>
2024-07-08 23:50:33 -0300

    fix: test_pipeline.py

--- app/pipeline/extract.py
+++ app/pipeline/extract.py
@@ -1,31 +1,31 @@
-import os # biblioteca para manipular arquivos e pastas
-import glob # biblioteca para listar arquivos
+"""esse é o módulo de extração de dados."""
 
-import pandas as pd
+import glob  # biblioteca para listar arquivos
+import os  # biblioteca para manipular arquivos e pastas
 from typing import List
 
-"""
-função para ler os arquivos de uma pasta data/input e retornar uma lista de dataframes
+import pandas as pd
 
-args: input_path (str): caminho da pasta com os arquivos
-
-return: lista de dataframes
-"""
-
-
-
-path = "data/input"
 
 def extract_from_excel(path: str) -> List[pd.DataFrame]:
+    """
+    função para ler os arquivos de
+    uma pasta data/input e retornar uma lista de dataframes
+
+    args: input_path (str): caminho da pasta com os arquivos
+
+    return: lista de dataframes
+    """
     all_files = glob.glob(os.path.join(path, "*.xlsx"))
 
     data_frame_list = []
     for file in all_files:
-        data_frame_list.append(pd.read_excel(file))
-    
+        data = pd.read_excel(file)
+        data_frame_list.append(data)
 
     return data_frame_list
 
+
 if __name__ == "__main__":
-    data_frame_list = extract_from_excel(path)
+    data_frame_list = extract_from_excel("data/input")
     print(data_frame_list)
--- app/pipeline/transform.py
+++ app/pipeline/transform.py
@@ -1,8 +1,10 @@
-import pandas as pd
 from typing import List
-"""
-função para transformar uma lista de dataframes em único dataframe
-"""
 
-def concat_data_frames(data_frames_list: List[pd.DataFrame]) -> pd.DataFrame:
-    return pd.concat(data_frames_list, ignore_index=True)
+import pandas as pd
+
+
+def contact_data_frames(data_frame_list: List[pd.DataFrame]) -> pd.DataFrame:
+    """
+    função para transformar uma lista de dataframes em um único dataframe.
+    """
+    return pd.concat(data_frame_list, ignore_index=True)
