9ab157fa3ef651f6b526d0d9dc571cb19790ad5f
Wenderson <wenderson_fagundes@hotmail.com>
2024-07-10 23:25:31 -0300

    feat:requests

--- app/pipeline/load.py
+++ app/pipeline/load.py
@@ -14,7 +14,7 @@ def load_excel(data_frame: pd.DataFrame, output_path: str, file_name: str) -> st
     return: "Arquivo salvo com sucesso
     """ 
     if not os.path.exists(output_path):
-        os.makedirs(out)
+        os.makedirs(output_path)
     data_frame.to_excel(f"{output_path}/{file_name}.xlsx", index=False)
     return "Arquivo salvo com sucesso"
 
---
+++ app/teste_request.py
@@ -0,0 +1,9 @@
+import requests 
+
+api_pokemon = 'https://pokeapi.co/api/v2/pokemon/1'
+
+
+r = requests.get(api_pokemon)
+r.status_code
+
+assert r.status_code == 200
